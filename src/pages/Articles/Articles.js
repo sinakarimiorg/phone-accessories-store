d21e4ebc1a35075f20449c5d1c2b2ffd4d85354d
sina karimi <karimisinaofficial@gmail.com>
2025-01-10 15:50:09 +0330

    develop articles page with responsive featur

--- src/pages/Articles/Articles.js
+++ src/pages/Articles/Articles.js
@@ -22,11 +22,20 @@ export default function Articles() {
               <MdFormatListNumberedRtl className='w-5 h-5' />
               مرتب سازی براساس:
             </div>
+            <div className='hidden sm:flex-center gap-x-8'>
             <a href='#'>جدید ترین</a>
             <a href='#'>قدیمی ترین</a>
+            </div>
+            <div className='sm:hidden'>
+              <select className='px-3 py-1 bg-gray-200 text-sm cursor-pointer focus:outline-none rounded-md'>
+                <option><a href='#'>جدید ترین</a></option>
+                <option><a href='#'>قدیمی ترین</a></option>
+              </select>
+            </div>
           </div>
+          
 
-          <div className='grid grid-cols-4 gap-x-10 gap-y-10'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 custom-sc:grid-cols-3 xl:grid-cols-4 gap-x-10 gap-y-10'>
             <ArticleBox shortName={'best-phone'} img={'/images/articles/phone.jpg'} title={'بهترین گوشی تا پنج میلیون'} date={[{ day: '21', month: 'تیر', year: '1404' }]} />
             <ArticleBox shortName={'earn-from-game'} img={'/images/articles/game-article.jpg'} title={'کسب درآمد از بازی!'} date={[{ day: '03', month: 'آذر', year: '1404' }]} />
             <ArticleBox shortName={'how-buy-speaker'} img={'/images/articles/speaker.jpg'} title={'راهنمای خرید اسپیکر بلوتوث قابل حمل (10 نکته ی مهم ضروری و کاربردی)'} date={[{ day: '28', month: 'اردیبهشت', year: '1403' }]} />
