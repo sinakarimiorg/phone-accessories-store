187061b66de93e7e0d2c08db52e5def6dd999522
sina karimi <karimisinaofficial@gmail.com>
2025-02-07 19:14:45 +0330

    make admin panel products page full responsive

--- src/pages/AdminPanel/Products/Products.js
+++ src/pages/AdminPanel/Products/Products.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import Swal from 'sweetalert2';
+import { MdOutlineVisibility } from 'react-icons/md'
 
 import './Products.css'
 
 export default function Products() {
+
+    // Show Product Infos 
+    const showProductInfos = (product) => {
+      Swal.fire({
+        title: `اطلاعات ${product.name}`,
+        html: `
+          <p><strong>نام کاربری:</strong> ${product.productname}</p>
+          <p><strong>شماره:</strong> ${product.phone}</p>
+          <p><strong>نقش:</strong> ${product.role}</p>
+        `,
+        icon: 'info',
+        confirmButtonText: 'باشه',
+      });
+    };
   return (
     <div>
       {/* Top Row & Add Product Section */}
       <div>
         <h1 className='p-admin__title'>افزودن کـالا</h1>
-        <div className='grid grid-cols-2 gap-y-7 gap-x-10'>
+        <div className='grid grid-cols-1 xs:grid-cols-2 gap-y-7 gap-x-6 md:gap-x-10'>
           <div>
             <label>نام کالا</label>
             <input className='p-admin__input' type='text' placeholder='لطفا نام کالا را وارد کنید...' />
@@ -27,11 +43,11 @@ export default function Products() {
           </div>
           <div>
             <label>توضیحات کالا</label>
-            <textarea className='w-full h-24 px-3 py-3 text-[10px] md:text-xs lg:text-sm focus:outline-none rounded-md' type='text' placeholder='لطفا توضیحات کالا را وارد کنید...' />
+            <textarea className='w-full h-20 lg:h-24 px-1 custom-sc:px-3 py-3 text-[10px] md:text-xs lg:text-xs custom-sc:text-sm tracking-tighter focus:outline-none rounded-md' type='text' placeholder='لطفا توضیحات کالا را وارد کنید...' />
           </div>
           <div>
             <label>کاور کالا</label>
-            <input className='' type='file' placeholder='لطفا کاور کالا را وارد کنید...' />
+            <input className='text-xs custom-sc:text-sm tracking-tighter lg:text-base' type='file' placeholder='لطفا کاور کالا را وارد کنید...' />
           </div>
         </div>
         <div className='w-[65%] mx-auto mt-5 py-3 text-center text-white tracking-wide bg-custom-navy/90 rounded-md cursor-pointer'>افـزودن</div>
@@ -40,14 +56,15 @@ export default function Products() {
       {/* Bottom Row & Products Section */}
       <div className='p-admin__box mt-20'>
       <h1 className='p-admin__box-title'>لیست کـالا‌ها</h1>
-      <table className='w-full'>
+      <table className='w-full text-sm lg:text-base'>
               <thead className='bg-custom-navy/15'>
                 <tr>
-                  <th>شناسه</th>
-                  <th>دسته بندی</th>
+                  <th className='hidden xs:table-cell'>شناسه</th>
+                  <th className='hidden xl:table-cell'>دسته بندی</th>
                   <th>عنوان</th>
-                  <th>مبلغ</th>
-                  <th>موجودی</th>
+                  <th className='hidden md:table-cell'>مبلغ</th>
+                  <th className='hidden custom-sc:table-cell'>موجودی</th>
+                  <th className='table-cell md:hidden'>مشاهده</th>
                   <th>ویرایش</th>
                   <th>حذف</th>
                 </tr>
@@ -55,146 +72,157 @@ export default function Products() {
 
               <tbody>
                 <tr>
-                  <td>1</td>
+                  <td className='hidden xs:table-cell'>1</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>2</td>
+                  <td className='hidden xs:table-cell'>2</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000,123</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000,123</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>3</td>
+                  <td className='hidden xs:table-cell'>3</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>4</td>
+                  <td className='hidden xs:table-cell'>4</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>5</td>
+                  <td className='hidden xs:table-cell'>5</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>6</td>
+                  <td className='hidden xs:table-cell'>6</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>7</td>
+                  <td className='hidden xs:table-cell'>7</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>8</td>
+                  <td className='hidden xs:table-cell'>8</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>9</td>
+                  <td className='hidden xs:table-cell'>9</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>10</td>
+                  <td className='hidden xs:table-cell'>10</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
                 <tr>
-                  <td>11</td>
+                  <td className='hidden xs:table-cell'>11</td>
                   <td>کاور چرمی سامسونگ s23 fe - همراه با درب محافظ</td>
-                  <td>کاور و قاب</td>
-                  <td>450,000</td>
-                  <td>123</td>
+                  <td className='hidden xl:table-cell'>کاور و قاب</td>
+                  <td className='hidden md:table-cell'>450,000</td>
+                  <td className='hidden custom-sc:table-cell'>123</td>
+                  <td className='table-cell md:hidden'><MdOutlineVisibility onClick={() => showProductInfos()} className='w-4 h-4 mx-auto cursor-pointer' /></td>
                   <td>
-                    <button className='px-3 py-1.5 text-sm text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
+                    <button className='px-1 custom-sc:px-3 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-custom-navy/90 rounded-md'>ویرایش</button>
                   </td>
                   <td>
-                    <button className='px-3.5 py-1.5 text-sm text-white bg-red-600 rounded-md'>حذف</button>
+                    <button className='px-1 custom-sc:px-3.5 py-1.5 text-xs custom-sc:text-sm tracking-tighter text-white bg-red-600 rounded-md'>حذف</button>
                   </td>
                 </tr>
               </tbody>
